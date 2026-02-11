a7608b71f10f1e0f8592479e9934203b82d7842f
itshitanshu-byte <itshitanshu@gmail.com>
2026-02-11 21:10:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Get elements
 const envelopeContainer = document.getElementById("envelope-container");
 const letterContainer = document.getElementById("letter-container");
 const yesBtn = document.querySelector(".yes-btn");
@@ -10,8 +9,9 @@ const buttons = document.getElementById("letter-buttons");
 const finalText = document.getElementById("final-text");
 const music = document.getElementById("loveMusic");
 
-// Hide letter at start
+// Hide letter initially
 letterContainer.style.display = "none";
+finalText.style.display = "none";
 
 // Open letter
 envelopeContainer.addEventListener("click", () => {
@@ -19,20 +19,18 @@ envelopeContainer.addEventListener("click", () => {
   letterContainer.style.display = "flex";
 });
 
-// Move NO button randomly
+// Move NO button
 noBtn.addEventListener("mouseover", () => {
   const x = Math.random() * 300 - 150;
   const y = Math.random() * 300 - 150;
   noBtn.style.transform = `translate(${x}px, ${y}px)`;
 });
 
-// YES button clicked
+// YES click
 yesBtn.addEventListener("click", () => {
   title.textContent = "Yippeeee! ❤️";
   cat.src = "cat_dance.gif";
-
   buttons.style.display = "none";
   finalText.style.display = "block";
-
   music.play();
 });
